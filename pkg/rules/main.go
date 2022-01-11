ac618e381c4df7e386f6774ff671c3cd2cc4f1b8
Federico Gimenez <federico.gimenez@gmail.com>
2022-01-11 19:27:09 +0100

    implement rules.Apply

--- pkg/rules/main.go
+++ pkg/rules/main.go
@@ -2,6 +2,9 @@ package rules
 
 import (
 	"encoding/csv"
+	"fmt"
+	"io"
+	"strings"
 
 	"github.com/spf13/afero"
 	"gopkg.in/yaml.v2"
@@ -20,7 +23,7 @@ type RenameColumnRule struct {
 }
 
 type DropRowRule struct {
-	Regexp string `yaml:"regexp"`
+	Substring string `yaml:"substring"`
 }
 
 type Rules struct {
@@ -45,5 +48,58 @@ func Parse(cfg *config.Config) (*Rules, error) {
 }
 
 func (r *Rules) Apply(input csv.Reader, output csv.Writer) error {
-	return nil
+	row := 0
+	writeRecord := true
+	for {
+		record, err := input.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		for _, rule := range r.SwapColumn {
+			if len(record) < rule.OriginIndex {
+				return fmt.Errorf("Swap column origin index out of bounds %d\n", rule.OriginIndex)
+			}
+			if len(record) < rule.TargetIndex {
+				return fmt.Errorf("Swap column target index out of bounds %d\n", rule.TargetIndex)
+			}
+
+			record[rule.TargetIndex], record[rule.OriginIndex] = record[rule.OriginIndex], record[rule.TargetIndex]
+		}
+
+		for _, rule := range r.RenameColumn {
+			if len(record) < rule.Index {
+				return fmt.Errorf("Rename column index out of bounds %d\n", rule.Index)
+			}
+
+			if rule.Name == "" {
+				return fmt.Errorf("Rename column empty name")
+			}
+
+			if row == 0 {
+				record[rule.Index] = rule.Name
+			}
+		}
+
+		for _, rule := range r.DropRow {
+			if strings.Contains(strings.Join(record, ","), rule.Substring) {
+				writeRecord = false
+				continue
+			}
+		}
+
+		if writeRecord {
+			err = output.Write(record)
+			if err != nil {
+				return err
+			}
+		}
+		writeRecord = true
+		row++
+	}
+	output.Flush()
+	return output.Error()
 }
